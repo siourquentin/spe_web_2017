57dd8c385a6d4955a43742d2c34f1b9e4dcbc662
siourquentin <siourquentin@gmail.com>
2017-02-24 16:03:08 +0100

    ajout des autres fichiers

---
+++ src/components/menu/menu.js
@@ -0,0 +1,13 @@
+var $ = require('jquery');
+
+var menu = {
+	init : function()
+	{
+		// $('header').css({
+		// 	'text-align' : 'center',
+		// 	'font-size' : '20px'
+		// });
+	}
+}
+
+module.exports = menu; 
